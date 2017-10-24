f8ad349109f8e9b78139c110df192d47dfc4d535
shiishii <sherif.elbishlawi@gmail.com>
2017-10-23 20:36:32 -0400

    Added Recording and View Recording Functionality
    Added Record button that records using phone app and added a listener to viewRecording button to open a video from media gallery

--- app/src/main/java/com/example/courtney/gauss/MainActivity.java
+++ app/src/main/java/com/example/courtney/gauss/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.courtney.gauss;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Gallery;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_VIDEO_CAPTURE = 1;
 
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            mVideoView.setVideoURI(videoUri);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
                 " \nConnect or view saved content.", Toast.LENGTH_LONG).show();
 
 
+        Button record = (Button)findViewById(R.id.record_video_btn);
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dispatchTakeVideoIntent();
+
+            }
+        });
         Button wifi = (Button)findViewById(R.id.find_network_btn);
         wifi.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button viewRecordings = (Button)findViewById(R.id.view_recordings_btn);
+        viewRecordings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
 
 
         Button connectedView = (Button)findViewById(R.id.connected_view_btn);
